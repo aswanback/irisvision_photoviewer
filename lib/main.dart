d0491d116508114ac57704fd7e5fd93945d63701
andrewswanback <atswanback@gmail.com>
2021-03-14 17:17:15 -0400

    testing git branching

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'LoadingScreen.dart';
-
+//test
 void main() => runApp(App());
 
 class App extends StatefulWidget {
